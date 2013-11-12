0d72c23aa49f2b3a350802d5c7a9fe74cee09209
Ankeet Mutha <ankeet.mutha@students.olin.edu>
2013-11-12 12:28:38 -0500

    Make Trade function better

--- trade.py
+++ trade.py
@@ -2,5 +2,10 @@ import takeCard
 import payCard 
 import player
 
-def Trade(player1,resources1, player2, resources2):
-    player1.hand
+def trade(player1,resources1, player2, resources2):
+    for i in range(len(resources1-1)):
+        payCard(player1,resource[i])
+        takeCard(player2,resource[i])
+    for i in range(len(resources2-1)):
+        payCard(player2,resource[i])
+        takeCard(player1,resource[i])
