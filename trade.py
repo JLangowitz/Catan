916169c0c72292e1d0804908cd7ee26d387b7efb
Ankeet Mutha <ankeet.mutha@students.olin.edu>
2013-11-13 00:13:25 -0500

    Removed Scaffolding

--- trade.py
+++ trade.py
@@ -2,10 +2,6 @@ import player
 from payCard import payCard
 from takeCard import takeCard
 
-player1 = player.Player()
-player2 = player.Player()
-player1.hand["sheep"] = 3
-player2.hand["ore"] = 1
 
 def trade(player1,resources1, player2, resources2):
     """ Commits a trade between two players. May be able to 
@@ -22,6 +18,3 @@ def trade(player1,resources1, player2, resources2):
         payCard(player2,resources2[i])
         takeCard(player1,resources2[i])
 
-trade(player1,["sheep","sheep","sheep"],player2,["ore"])
-print player1.hand
-print player2.hand
