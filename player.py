94be307459dc8167400ad96eca493798908fcb88
Ankeet Mutha <ankeet.mutha@students.olin.edu>
2013-11-13 01:41:42 -0500

    Allows player to get resources

--- player.py
+++ player.py
@@ -12,6 +12,8 @@ class Player:
         self.buildings = []
         self.soldiers = 0
         self.devcards = {}
+        #self.hist is a dic mapping dice roll to cards a person goes        
+        self.hist{2:[],3:[],4:[],5:[],6:[],8:[],9:[],10:[],11:[],12[]}
 
     def __str__(self):
         if len(self.buildings) == 0:
@@ -34,9 +36,9 @@ Longest Road? Largest Army?
         points = bonusPoints
 
 def main():
-    n = raw_input("Number of Players (max 5)? ")
+    n = raw_input("Number of Players (max 4)? ")
     n = int(n)
-    while n <= 0 or n > 5:
+    while n <= 0 or n > 4:
         n = raw_input("Try again ")
         n = int(n)
     player1 = Player()
