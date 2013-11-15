d8d609a53e866b9185da30825c4e18a1e1951d57
JLangowitz <joshua.langowitz@students.olin.edu>
2013-11-15 12:24:11 -0500

    the web

---
+++ CatanServer/app.py
@@ -0,0 +1,31 @@
+"""
+SofDes F2013
+Team Catan
+Emily Guthrie, Josh Langowitz, Ankeet Mutha, Brooks Willis
+Website
+"""
+# imports
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+import os
+from werkzeug import secure_filename
+
+# config
+DEBUG = True
+SECRET_KEY = 'i6TIU_oI7yoiHp_p9Ppu_oGTGiu6Fdy5ufr__sda-3'
+USERNAME = 'toomanybricksonthedancefloor'
+PASSWORD = 'default'
+
+# app setup
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+
+@app.route('/cut/<filename>', methods=['GET','POST'])
+
+@app.route('/')
+def homepage():
+    return render_template('index.jade', title='Catan')
+
+if __name__ == '__main__':
+    app.run()
