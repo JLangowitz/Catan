c65fb4644536e604d781500b7ed00692c536a8d2
Emily <emily.guthrie@students.olin.edu>
2013-11-15 12:18:05 -0500

    Making the setup for the board

--- gameObjects.py
+++ gameObjects.py
@@ -1,3 +1,4 @@
+
 class Board(object):
     """Contains Board and all tile and vertex position
 
@@ -17,6 +18,11 @@ class Hexes(object):
 
     attributes: str resource type, int roll number, boolean robber status
     """
+    def __init__(self,coordinates=(0,0),resource='',rollNumber=0,robber=False):
+        self.coordinates=coordinates
+        self.resource=resource
+        self.rollNumber=rollNumber
+        self.robber=robber
 class Structure(object):
     """Represents every structure on the board
 
@@ -24,10 +30,29 @@ class Structure(object):
     attributes: player class, boolean, ifCity, vertex
     """
 
-class Player(object):
-    """Represents every player
+
+def setup(numPlayers):
+    """Setsup all the board objects and establishes relationships and values"""
+    if numPlayers<3 or numPlayers>5:
+        return 'Too many or too few players specified'
+    if 2<numPlayers<5:
+        resources={}
+        resources['wood']=4
+        resources['wheat']=4
+        resources['Sheep']=4
+        resources['brick']=3
+        resources['ore']=3
+        resources['desert']=1
+        ports=[]
 
 
-    attributes: int points, dict bonuses, dict hand, list of Structures,
-    int Soldiers counter
-    """
+
+    if 4<numPlayers<7:
+        resources={}
+        resources['wood']=6
+        resources['wheat']=6
+        resources['Sheep']=6
+        resources['brick']=5
+        resources['ore']=5
+        resources['desert']=2
+        ports=[]
