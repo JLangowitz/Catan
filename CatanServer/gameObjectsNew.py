090b7f4008271104ed4af4c6af2f79821f3a1acd
Ankeet Mutha <ankeet.mutha@students.olin.edu>
2013-12-04 22:11:27 -0500

    Added more port stuff

--- CatanServer/gameObjectsNew.py
+++ CatanServer/gameObjectsNew.py
@@ -46,7 +46,8 @@ class Vertex(object):
         self.hexes=h
         self.built=False
         self.neighbors=neighbors
-    
+        self.ports = "none"
+
     def build(self):
         self.built = True
 
