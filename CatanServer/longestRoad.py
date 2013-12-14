f5cd2d6eb6d00c70438d1caad75512e5aba6d54a
Brooks Willis <christopher.willis@students.olin.edu>
2013-12-14 02:37:59 -0500

    Started implementing breadth first search for longest road

--- CatanServer/longestRoad.py
+++ CatanServer/longestRoad.py
@@ -1,4 +1,6 @@
 from collections import Counter
+from gameObjectsNew import *
+from player import *
 def longestRoad(players):
     maxRoad = 4
     winner = None
@@ -11,16 +13,16 @@ def longestRoad(players):
 
 def roadLength(player):
     verticies = []
-    for road in player.getRoads()
+    for road in player.getRoads():
         verticies.append(road[0])
         verticies.append(road[1]) 
     vertHist = Counter(verticies)
     starts = []
     forks = []
     for vert in vertHist:
-        if vertHist[vert] = 1:
+        if vertHist[vert] == 1:
             starts.append(vert)
-        if vertHist[vert] = 3:
+        if vertHist[vert] == 3:
             forks.append(vert)
 
     if len(forks) == 0:
@@ -37,45 +39,34 @@ def roadLength(player):
             memo.append(queue[0])
             neighbors = queue[0].getNeighbors()
             for neighbor in neighbors:
-                if :
-
-def DFS_recursive(G,v):
-    memo.append(v)
-    neighbors = queue[0].getNeighbors()
-        for neighbor in neighbors:
-            if neighbor not in explored:
-                w ← G.adjacentVertex(v,neighbor)
-                if w not in explored:
-                    memo.append(neighbor)
-                    DFS_recursive(G,w)
-
-                    
-                
-def DFS-iterative(G,v):
-2      label v as discovered
-3      let S be a stack
-4      S.push(v)
-5      while S is not empty        
-6            t ← S.peek() 
-7            if t is what were looking for: 
-8                return t
-9            for all edges e in G.adjacentEdges(t) do
-10               if edge e is already labelled 
-11                   continue with the next edge
-12               w ← G.adjacentVertex(t,e)
-13               if vertex w is not discovered and not explored
-14                   label e as tree-edge
-15                   label w as discovered
-16                   S.push(w)
-17                   continue at 5
-18               else if vertex w is discovered
-19                   label e as back-edge
-20               else
-21                   // vertex w is explored
-22                   label e as forward- or cross-edge
-23           label t as explored
-24           S.pop()
+                pass
 
 
+def BFS(coordhist, start):
+    memo = start
+    paths = {}
+    for neighbor in start.getNeighbors():
+        paths[neighbor] = [start]
 
 
+    for path in paths:
+        if path not in memo:
+            prev = paths[path]
+            del paths[path]
+            newPath = prev.append(path)
+            for neighbor in path.getNeighbors():
+                if neighbor not in memo:
+                    path[neighbor] = newPath
+
+
+def main():
+    p1 = Player()
+    p2 = Player()
+    p3 = Player()
+    p4 = Player()
+    game = Game([p1,p2,p3,p4])
+    print game.board.printVerticies()
+
+
+if __name__ == '__main__':
+    main()
