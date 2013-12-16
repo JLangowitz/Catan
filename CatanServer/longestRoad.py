4da1a4286900df0ca08efc09956678a3d37b95d4
Brooks Willis <christopher.willis@students.olin.edu>
2013-12-15 23:33:46 -0500

    Longest road isolated in single script incase of debugging need

--- CatanServer/longestRoad.py
+++ CatanServer/longestRoad.py
@@ -1,11 +1,14 @@
 from collections import Counter
-from gameObjectsNew import *
-from player import *
-def longestRoad(players):
+import gameObjectsNew
+import player
+import copy
+
+
+def longestRoad(vertMap, players):
     maxRoad = 4
     winner = None
     for player in players:
-        playerMaxRoad = roadLength(player)
+        playerMaxRoad = roadLength(vertMap, player)
         if playerMaxRoad == maxRoad:
             winner == None
         elif playerMaxRoad > maxRoad:
@@ -21,23 +24,17 @@ def roadLength(vertMap, player):
         verticies.append(road[0])
         verticies.append(road[1]) 
     vertHist = Counter(verticies)
-    starts = []
-    forks = []
-    for vert in vertHist:
-        if vertHist[vert] == 1:
-            starts.append(vert)
-        if vertHist[vert] == 3:
-            forks.append(vert)
 
-    if len(forks) == 0:
-        return len(roads)
+    maxRoadLength = 0
 
-    if len(starts) == 0 and len(forks) == 2:
-        return len(roads)
-    print findLongest(vertMap, vertHist, [[(1.5,-1.5)]])
+    for vertex in vertHist:
+        roadPath = findLongest(vertMap, vertHist, [[vertex]])
+        roadLength = len(roadPath[0]) - 1
+        print 'longest path for', vertex, 'is', roadPath, 'and is of length', len(roadPath[0]) - 1
+        if roadLength > maxRoadLength:
+            maxRoadLength = roadLength
 
-    #for vertex in vertHist:
-        #print 'longest path for', vertex, 'is', findLongest(vertMap, vertHist, [[vertex]])
+    return maxRoadLength
 
 
 def findLongest(vertMap, vertHist, oldPaths):
@@ -56,35 +53,28 @@ def extend(vertMap, vertHist, path):
     #print path
 
     neighbors = vertMap[path[-1]]
-    print 'path end', path[-1]
-    print 'neighbors', neighbors
+    #print 'path end', path[-1]
+    #print 'neighbors', neighbors
     for neighbor in neighbors:
         #print 'neigh', neighbor
-        temp = path
+        temp = copy.deepcopy(path)
         if neighbor in vertHist and neighbor not in path:
             temp.append(neighbor)
             newPaths += [temp]
             
-    print 'newpaths', newPaths
+    #print 'newpaths', newPaths
     return newPaths
 
 
 def main():
-    p1 = Player()
-    p2 = Player()
-    p3 = Player()
-    p4 = Player()
+    p1 = player.Player()
+    p2 = player.Player()
+    p3 = player.Player()
+    p4 = player.Player()
     players = [p1, p2, p3, p4]
-    game = Game(players)
+    game = gameObjectsNew.Game(players)
     vertMap = game.board.vertexMap()
-    """
-    p1.roads = [[game.getVertex((0.5,-1.5)),game.getVertex((0.5,-2.0))]]
-    p1.roads.append([game.getVertex((0.5,-2.0)),game.getVertex((0.5,-2.5))])
-    p1.roads.append([game.getVertex((0.5,-2.5)),game.getVertex((-0.5,-2.5))])
-    p1.roads.append([game.getVertex((0.5,-2.0)),game.getVertex((1.5,-2.0))])
-    p1.roads.append([game.getVertex((1.5,-2.0)),game.getVertex((1.5,-1.5))])
-    p1.roads.append([game.getVertex((-0.5,-2.5)),game.getVertex((-0.5,-2.0))])
-    """
+   
     p1.roads = [[(0.5,-1.5),(0.5,-2.0)]]
     p1.roads.append([(0.5,-2.0),(0.5,-2.5)])
     p1.roads.append([(0.5,-2.5),(-0.5,-2.5)])
@@ -94,44 +84,9 @@ def main():
     p1.roads.append([(-0.5,-2.0),(-0.5,-1.5)])
     p1.roads.append([(-0.5,-1.5),(0.5,-1.5)])
     p1.roads.append([(0.5,-1.5),(0.5,-1.0)])
+    p1.soldiers = 1
 
-    roadLength(vertMap, p1)
+    print longestRoad(vertMap, players)
 
 if __name__ == '__main__':
     main()
-
-
-
-"""
-    for vertex in vertHist: #depth first search
-        memo = [vertex]
-        explored = []
-        queue = [vertex]
-        while len(queue) > 0:
-            memo.append(queue[0])
-            neighbors = queue[0].getNeighbors()
-            for neighbor in neighbors:
-                pass
-"""
-
-"""
-def bfs(game, coordhist, start):
-    memo = [start]
-    paths = {}
-    for neighbor in start.getNeighbors():
-        neighbor = game.getVertex(neighbor)
-        print 'neigh', neighbor
-        paths[neighbor] = [start]
-
-
-    for path in paths:
-        if path not in memo:
-            print 'path', path
-            prev = paths[path]
-            del paths[path]
-            newPath = prev.append(path)
-            print "new", newPath
-            for neighbor in path.getNeighbors():
-                if neighbor not in memo:
-                    paths[neighbor] = newPath
-"""
